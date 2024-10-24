143a68faa1f2de057695c33665d3d1ad977ae4bb
José Manuel Luque <jluqgon214@g.educaand.es>
2024-10-24 13:58:46 +0200

    Creacion

---
+++ src/main/kotlin/Java.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Ejercicio1();
+}
+
+public class Ejercicio1 {
+    Scanner sc = new Scanner(System.in);
+}
---
+++ src/main/kotlin/Kotlin.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+}
+
+fun Ejercicio1() {
+    println("Escriba su nombre:")
+    val nombre = readln()
+    println("¡Hola $nombre!")
+}
